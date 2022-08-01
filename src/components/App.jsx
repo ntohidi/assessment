4373489add8acdca76ed84104bcd45d783b55cde
ntohidi <nasrin@kidocode.com>
2022-08-01 10:35:00 +0800

    read posts data from the api, added table component, added pagination

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import Table from './Table';
+
+
 function App() {
-  return <div>{/* Complete the exercise here. */}</div>;
+  const [posts, setData] = useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [pageCount, setPageCount] = React.useState(0);
+  const fetchIdRef = React.useRef(0);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = () => {
+    fetch('/api/posts')
+      .then((res) => res.json())
+      .then((data) => {
+        data.posts.forEach((post) => {
+          post.name = `${post.author.name}`;
+          post.avatar = `${post.author.avatar}`;
+          delete post.author;
+        });
+        setData(data.posts);
+      })
+      .catch((error) => console.log('Error fetching posts', error));
+  };
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Title',
+        accessor: 'title',
+      },
+      {
+        Header: 'Name',
+        accessor: 'name',
+      },
+      {
+        Header: 'Avatar',
+        accessor: 'avatar',
+      },
+      {
+        Header: 'Publish Date',
+        accessor: 'publishDate',
+      },
+      {
+        Header: 'Summary',
+        accessor: 'summary',
+      },
+    ],
+    []
+  );
+
+  // This will get called when the table needs new data
+  const fetchData = React.useCallback(({ pageSize, pageIndex }) => {
+    // Give this fetch an ID
+    const fetchId = ++fetchIdRef.current;
+
+    // Set the loading state
+    setLoading(true);
+
+    // Only update the data if this is the latest fetch
+    if (fetchId === fetchIdRef.current) {
+      const startRow = pageSize * pageIndex;
+      const endRow = startRow + pageSize;
+      setData(posts.slice(startRow, endRow));
+
+      setPageCount(Math.ceil(posts.length / pageSize));
+
+      setLoading(false);
+    }
+  }, []);
+
+  return (
+    <section className='table-holder'>
+      <Table
+        columns={columns}
+        data={posts}
+        fetchData={fetchData}
+        loading={loading}
+        pageCount={pageCount}
+      />
+    </section>
+  );
 }
 
 export default App;
---
+++ src/components/Table.jsx
@@ -0,0 +1,131 @@
+import React, { useEffect } from 'react';
+import { useTable, useSortBy, usePagination } from 'react-table';
+
+const pageCount = 10;
+
+const Table = ({
+  columns,
+  data,
+  fetchData,
+  loading,
+  pageCount: controlledPageCount,
+}) => {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize, sortBy },
+  } = useTable(
+    {
+      columns,
+      data,
+      manualPagination: true,
+      manualSortBy: true,
+      autoResetPage: false,
+      autoResetSortBy: false,
+      pageCount: controlledPageCount,
+    },
+    useSortBy,
+    usePagination
+  );
+
+  useEffect(() => {
+    fetchData({ pageIndex, pageSize });
+  }, [fetchData, pageIndex, pageSize]);
+
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()} key="header">
+              {headerGroup.headers.map((column) => (
+                <th>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()} id={row.id} key={row.id}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+          <tr>
+            {loading ? (
+              // Use our custom loading state to show a loading indicator
+              <td colSpan="10000">Loading...</td>
+            ) : (
+              <td colSpan="10000">
+                Showing {page.length} of ~{controlledPageCount * pageSize}{' '}
+                results
+              </td>
+            )}
+          </tr>
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(page);
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
+};
+
+export default Table;
